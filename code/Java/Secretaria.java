b4e305cb72b0a2cd9566d9906d011a263759a293
Lucas Gonçalves dos Santos <106043567+devglucas@users.noreply.github.com>
2025-02-23 22:41:35 -0300

    Add files via upload

---
+++ code/Java/Secretaria.java
@@ -0,0 +1,61 @@
+import java.util.List;
+
+public class Secretaria {
+
+    private List<Usuario> usuarios;
+    private List<Aluno> alunos;
+    
+    // Métodos
+    public void adicionarProfessores() {
+        // Implementação
+    }
+    
+    public void removerProfessores(String matricula) {
+        // Implementação 
+    }
+    
+    public void editarProfessores(String matricula) {
+        // Implementação 
+    }
+    
+    public void adicionarDisciplinas() {
+        // Implementação 
+    }
+    
+    public void removerDisciplinas(int id) {
+        // Implementação 
+    }
+    
+    public void editarDisciplinas(int id) {
+        // Implementação
+    }
+    
+    public void adicionarAlunos() {
+        // Implementação 
+    }
+    
+    public void removerAlunos(String matricula) {
+        // Implementação 
+    }
+    
+    public void editarAlunos(String matricula) {
+        // Implementação
+    }
+    
+    public Aluno aprovarMatricula(Aluno aluno, Disciplina disciplina) {
+        // Implementação 
+        return aluno;
+    }
+    
+    public void reprovarMatricula(Aluno aluno, Disciplina disciplina) {
+        // Implementação 
+    }
+    
+    public void cancelarDisciplina(Disciplina disciplina) {
+        // Implementação 
+    }
+    
+    public void gerarCurriculo() {
+        // Implementação 
+    }
+}
